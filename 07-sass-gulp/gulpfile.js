29a44e4c3029f2009e53d0e471dd2b209f8c63e8
Tino Reyna <tinoreyna1984@gmail.com>
2023-01-11 01:18:59 -0500

    Nuevos elementos de trabajo

--- 07-sass-gulp/gulpfile.js
+++ 07-sass-gulp/gulpfile.js
@@ -1,7 +1,23 @@
-function tarea(done) {
-    console.log("Mi primera tarea");
+const {src, dest, watch} = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const plumber = require("gulp-plumber");
+
+function css(done) {
+
+    src("src/scss/**/*.scss") // 1- identificar el archivo SASS
+    .pipe(plumber())
+    .pipe(sass()) // 2- compilar el archivo SASS a CSS
+    .pipe(dest("build/css")); // 3- guardar el CSS en disco duro
+
     done();
 }
 
-exports.tarea = tarea;
+function dev(done) {
+
+    watch("src/scss/**/*.scss", css);
+    done();
+}
+
+exports.css = css;
+exports.dev = dev;
 
